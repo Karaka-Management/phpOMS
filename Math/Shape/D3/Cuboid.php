357e672ca018dcf2ce5b2a421db62c0a7768c467
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-12-25 13:53:32 +0100

    Renaming shapes and implementing them

---
+++ Math/Shape/D3/Cuboid.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace phpOMS\Math\Algebra;
+
+class Cuboid
+{
+
+    /**
+     * Volume
+     *
+     * @param float $a Edge
+     * @param float $b Edge
+     * @param float $h Height
+     *
+     * @return float
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function getVolume(\float $a, \float $b, \float $h)
+    {
+        return $a * $b * $h;
+    }
+
+    /**
+     * Surface area
+     *
+     * @param float $a Edge
+     * @param float $b Edge
+     * @param float $h Height
+     *
+     * @return float
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function getSurface(\float $a, \float $b, \float $h)
+    {
+        return 2 * ($a * $b + $a * $h + $b * $h);
+    }
+}
---
+++ Math/Shape/D3/RectangularPyramid.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace phpOMS\Math\Algebra;
+
+class RectangularPyramid
+{
+
+    /**
+     * Volume
+     *
+     * @param float $a Edge
+     * @param float $b Edge
+     * @param float $h Height
+     *
+     * @return float
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function getVolume(\float $a, \float $b, \float $h)
+    {
+        return $a * $b * $h / 3;
+    }
+
+    /**
+     * Surface area
+     *
+     * @param float $a Edge
+     * @param float $b Edge
+     * @param float $h Height
+     *
+     * @return float
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function getSurface(\float $a, \float $b, \float $h)
+    {
+        return $a * $b + $a * sqrt(($b / 2) ** 2 + $h ** 2) + $b * sqrt(($a / 2) ** 2 + $h ** 2);
+    }
+
+    /**
+     * Lateral surface area
+     *
+     * @param float $a Edge
+     * @param float $b Edge
+     * @param float $h Height
+     *
+     * @return float
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function getLateralSurface(\float $a, \float $b, \float $h)
+    {
+        return $a * sqrt(($b / 2) ** 2 + $h ** 2) + $b * sqrt(($a / 2) ** 2 + $h ** 2);
+    }
+}
