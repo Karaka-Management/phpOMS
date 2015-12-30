a7d25081cd8de267757754fad01928be9f69784b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-12-30 22:50:26 +0100

    New admin abstractions

---
+++ Module/ActivateAbstract.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace phpOMS\Module;
+
+use phpOMS\DataStorage\Database\Pool;
+
+/**
+ * Installer Abstract class.
+ *
+ * @category   Framework
+ * @package    phpOMS\Module
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class ActivateAbstract
+{
+
+    /**
+     * Install module.
+     *
+     * @param Pool  $dbPool Database instance
+     * @param array $info   Module info
+     *
+     * @return void
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function activate(Pool $dbPool, array $info)
+    {
+    }
+}
