e90c2a3e717c406ff97051511467cc98654311aa
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-02-21 15:27:03 +0100

    Upcoming codes

---
+++ Utils/Barcode/Aztec.php
@@ -0,0 +1,24 @@
+<?php
+
+class Aztec
+{
+    private function calculateSize() {
+
+    }
+
+    private function bitStuffing() {
+
+    }
+
+    private function padding() {
+
+    }
+
+    private function check() {
+
+    }
+
+    private function arrange() {
+
+    }
+}
