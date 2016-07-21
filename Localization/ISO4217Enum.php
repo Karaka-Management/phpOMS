14e2e2b0cb4eb6d3da3ada9a42da0203854a2f83
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-07-21 18:36:29 +0200

    Reducing file size by removing empty lines

--- Localization/ISO4217Enum.php
+++ Localization/ISO4217Enum.php
@@ -31,230 +31,117 @@ use phpOMS\Datatypes\Enum;
 class ISO4217Enum extends Enum
 {
     const C_ALL = 'Albania, Leke';
-
     const C_AFN = 'Afghanistan, Afghanis';
-
     const C_ARS = 'Argentina, Pesos';
-
     const C_AWG = 'Aruba, Guilders';
-
     const C_AUD = 'Australia, Dollars';
-
     const C_AZN = 'Azerbaijan, New Manats';
-
     const C_BSD = 'Bahamas, Dollars';
-
     const C_BBD = 'Barbados, Dollars';
-
     const C_BYR = 'Belarus, Rubles';
-
     const C_BZD = 'Belize, Dollars';
-
     const C_BMD = 'Bermuda, Dollars';
-
     const C_BOB = 'Bolivia, Bolivianos';
-
     const C_BAM = 'Bosnia and Herzegovina, Convertible Marka';
-
     const C_BWP = 'Botswana, Pulas';
-
     const C_BGN = 'Bulgaria, Leva';
-
     const C_BRL = 'Brazil, Reais';
-
     const C_BND = 'Brunei Darussalam, Dollars';
-
     const C_KHR = 'Cambodia, Riels';
-
     const C_CAD = 'Canada, Dollars';
-
     const C_KYD = 'Cayman Islands, Dollars';
-
     const C_CLP = 'Chile, Pesos';
-
     const C_CNY = 'China, Yuan Renminbi';
-
     const C_COP = 'Colombia, Pesos';
-
     const C_CRC = 'Costa Rica, Colón';
-
     const C_HRK = 'Croatia, Kuna';
-
     const C_CUP = 'Cuba, Pesos';
-
     const C_CZK = 'Czech Republic, Koruny';
-
     const C_DKK = 'Denmark, Kroner';
-
     const C_DOP = 'Dominican Republic, Pesos';
-
     const C_XCD = 'East Caribbean, Dollars';
-
     const C_EGP = 'Egypt, Pounds';
-
     const C_SVC = 'El Salvador, Colones';
-
     const C_EEK = 'Estonia, Krooni';
-
     const C_EUR = 'Euro';
-
     const C_FKP = 'Falkland Islands, Pounds';
-
     const C_FJD = 'Fiji, Dollars';
-
     const C_GHC = 'Ghana, Cedis';
-
     const C_GIP = 'Gibraltar, Pounds';
-
     const C_GTQ = 'Guatemala, Quetzales';
-
     const C_GGP = 'Guernsey, Pounds';
-
     const C_GYD = 'Guyana, Dollars';
-
     const C_HNL = 'Honduras, Lempiras';
-
     const C_HKD = 'Hong Kong, Dollars';
-
     const C_HUF = 'Hungary, Forint';
-
     const C_ISK = 'Iceland, Kronur';
-
     const C_INR = 'India, Rupees';
-
     const C_IDR = 'Indonesia, Rupiahs';
-
     const C_IRR = 'Iran, Rials';
-
     const C_IMP = 'Isle of Man, Pounds';
-
     const C_ILS = 'Israel, New Shekels';
-
     const C_JMD = 'Jamaica, Dollars';
-
     const C_JPY = 'Japan, Yen';
-
     const C_JEP = 'Jersey, Pounds';
-
     const C_KZT = 'Kazakhstan, Tenge';
-
     const C_KES = 'Kenyan Shilling';
-
     const C_KGS = 'Kyrgyzstan, Soms';
-
     const C_LAK = 'Laos, Kips';
-
     const C_LVL = 'Latvia, Lati';
-
     const C_LBP = 'Lebanon, Pounds';
-
     const C_LRD = 'Liberia, Dollars';
-
     const C_LTL = 'Lithuania, Litai';
-
     const C_MKD = 'Macedonia, Denars';
-
     const C_MYR = 'Malaysia, Ringgits';
-
     const C_MUR = 'Mauritius, Rupees';
-
     const C_MXN = 'Mexico, Pesos';
-
     const C_MNT = 'Mongolia, Tugriks';
-
     const C_MZN = 'Mozambique, Meticais';
-
     const C_NAD = 'Namibia, Dollars';
-
     const C_NPR = 'Nepal, Rupees';
-
     const C_ANG = 'Netherlands Antilles, Guilders';
-
     const C_NZD = 'New Zealand, Dollars';
-
     const C_NIO = 'Nicaragua, Cordobas';
-
     const C_NGN = 'Nigeria, Nairas';
-
     const C_KPW = 'North Korea, Won';
-
     const C_NOK = 'Norway, Krone';
-
     const C_OMR = 'Oman, Rials';
-
     const C_PKR = 'Pakistan, Rupees';
-
     const C_PAB = 'Panama, Balboa';
-
     const C_PYG = 'Paraguay, Guarani';
-
     const C_PEN = 'Peru, Nuevos Soles';
-
     const C_PHP = 'Philippines, Pesos';
-
     const C_PLN = 'Poland, Zlotych';
-
     const C_QAR = 'Qatar, Rials';
-
     const C_RON = 'Romania, New Lei';
-
     const C_RUB = 'Russia, Rubles';
-
     const C_SHP = 'Saint Helena, Pounds';
-
     const C_SAR = 'Saudi Arabia, Riyals';
-
     const C_RSD = 'Serbia, Dinars';
-
     const C_SCR = 'Seychelles, Rupees';
-
     const C_SGD = 'Singapore, Dollars';
-
     const C_SBD = 'Solomon Islands, Dollars';
-
     const C_SOS = 'Somalia, Shillings';
-
     const C_ZAR = 'South Africa, Rand';
-
     const C_KRW = 'South Korea, Won';
-
     const C_LKR = 'Sri Lanka, Rupees';
-
     const C_SEK = 'Sweden, Kronor';
-
     const C_CHF = 'Switzerland, Francs';
-
     const C_SRD = 'Suriname, Dollars';
-
     const C_SYP = 'Syria, Pounds';
-
     const C_TWD = 'Taiwan, New Dollars';
-
     const C_THB = 'Thailand, Baht';
-
     const C_TTD = 'Trinidad and Tobago, Dollars';
-
     const C_TRY = 'Turkey, Lira';
-
     const C_TRL = 'Turkey, Liras';
-
     const C_TVD = 'Tuvalu, Dollars';
-
     const C_UAH = 'Ukraine, Hryvnia';
-
     const C_GBP = 'United Kingdom, Pounds';
-
     const C_USD = 'United States of America, Dollars';
-
     const C_UYU = 'Uruguay, Pesos';
-
     const C_UZS = 'Uzbekistan, Sums';
-
     const C_VEF = 'Venezuela, Bolivares Fuertes';
-
     const C_VND = 'Vietnam, Dong';
-
     const C_YER = 'Yemen, Rials';
-
     const C_ZWD = 'Zimbabwe, Zimbabwe Dollars';
 }
