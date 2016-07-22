d58ef6ee3bf9492679fffda7042622f36ebad7bc
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-07-22 16:19:40 +0200

    Currency sub unit added

---
+++ Localization/ISO4217SubUnitEnum.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace phpOMS\Localization;
+
+use phpOMS\Datatypes\Enum;
+
+/**
+ * Currency sub units ISO list.
+ *
+ * @category   Framework
+ * @package    phpOMS\Localization
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class ISO4217SubUnitEnum extends Enum
+{
+    const _AED = 100;
+    const _AFN = 100;
+    const _ALL = 100;
+    const _AMD = 100;
+    const _ANG = 100;
+    const _ARS = 100;
+    const _AUD = 100;
+    const _AWG = 100;
+    const _BAM = 100;
+    const _BBD = 100;
+    const _BDT = 100;
+    const _BGN = 100;
+    const _BHD = 1000;
+    const _BIF = 100;
+    const _BMD = 100;
+    const _BND = 100;
+    const _BOB = 100;
+    const _BRL = 100;
+    const _BSD = 100;
+    const _BTN = 100;
+    const _BWP = 100;
+    const _BYR = 100;
+    const _BZD = 100;
+    const _CAD = 100;
+    const _CDF = 100;
+    const _CHF = 100;
+    const _CLP = 100;
+    const _CNY = 100;
+    const _COP = 100;
+    const _CUC = 100;
+    const _CUP = 100;
+    const _CVE = 100;
+    const _CZK = 100;
+    const _DJF = 100;
+    const _DKK = 100;
+    const _DOP = 100;
+    const _DZD = 100;
+    const _EGP = 100;
+    const _ERN = 100;
+    const _EUR = 100;
+    const _FJD = 100;
+    const _FKP = 100;
+    const _GBP = 100;
+    const _GEL = 100;
+    const _GHS = 100;
+    const _GMD = 100;
+    const _GNF = 100;
+    const _GTQ = 100;
+    const _GYD = 100;
+    const _HKD = 100;
+    const _HNL = 100;
+    const _HRK = 100;
+    const _HTG = 100;
+    const _HUF = 100;
+    const _IDR = 100;
+    const _ILS = 100;
+    const _INR = 100;
+    const _IQD = 1000;
+    const _IRR = 100;
+    const _ISK = 100;
+    const _JMD = 100;
+    const _JOD = 100;
+    const _JPY = 100;
+    const _KES = 100;
+    const _KGS = 100;
+    const _KHR = 100;
+    const _KMF = 100;
+    const _KPW = 100;
+    const _KRW = 100;
+    const _KWD = 1000;
+    const _KYD = 100;
+    const _KZT = 100;
+    const _LAK = 100;
+    const _LBP = 100;
+    const _LKR = 100;
+    const _LRD = 100;
+    const _LSL = 100;
+    const _LTL = 100;
+    const _LVL = 100;
+    const _LYD = 1000;
+    const _MAD = 100;
+    const _MDL = 100;
+    const _MGA = 5;
+    const _MKD = 100;
+    const _MMK = 100;
+    const _MNT = 100;
+    const _MOP = 100;
+    const _MRO = 5;
+    const _MUR = 100;
+    const _MVR = 100;
+    const _MWK = 100;
+    const _MXN = 100;
+    const _MYR = 100;
+    const _MZN = 100;
+    const _NAD = 100;
+    const _NGN = 100;
+    const _NIO = 100;
+    const _NOK = 100;
+    const _NPR = 100;
+    const _NZD = 100;
+    const _OMR = 1000;
+    const _PAB = 100;
+    const _PEN = 100;
+    const _PGK = 100;
+    const _PHP = 100;
+    const _PKR = 100;
+    const _PLN = 100;
+    const _PYG = 100;
+    const _QAR = 100;
+    const _RON = 100;
+    const _RSD = 100;
+    const _RUB = 100;
+    const _RWF = 100;
+    const _SAR = 100;
+    const _SBD = 100;
+    const _SCR = 100;
+    const _SDG = 100;
+    const _SEK = 100;
+    const _SGD = 100;
+    const _SHP = 100;
+    const _SLL = 100;
+    const _SOS = 100;
+    const _SRD = 100;
+    const _SSP = 100;
+    const _STD = 100;
+    const _SVC = 100;
+    const _SYP = 100;
+    const _SZL = 100;
+    const _THB = 100;
+    const _TJS = 100;
+    const _TMT = 100;
+    const _TND = 1000;
+    const _TOP = 100;
+    const _TRY = 100;
+    const _TTD = 100;
+    const _TWD = 100;
+    const _TZS = 100;
+    const _UAH = 100;
+    const _UGX = 100;
+    const _USD = 100;
+    const _UYU = 100;
+    const _UZS = 100;
+    const _VEF = 100;
+    const _VND = 10;
+    const _VUV = None;
+    const _WST = 100;
+    const _XAF = 100;
+    const _XCD = 100;
+    const _XOF = 100;
+    const _XPF = 100;
+    const _YER = 100;
+    const _ZAR = 100;
+    const _ZMK = 100;
+    const _ZWL = 100;
+}
