85d43c57bd922583bf93c90f9b250520daed332f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-08-04 21:32:19 +0200

    Added character '-'

--- Uri/UriFactory.php
+++ Uri/UriFactory.php
@@ -109,7 +109,7 @@ class UriFactory
      */
     public static function build(string $uri, array $toMatch = [])
     {
-        return preg_replace_callback('(\{[\/#\?@\.\$][a-zA-Z0-9]*\})', function ($match) use ($toMatch) {
+        return preg_replace_callback('(\{[\/#\?@\.\$][a-zA-Z0-9\-]*\})', function ($match) use ($toMatch) {
             $match = substr($match[0], 1, strlen($match[0]) - 2);
 
             return $toMatch[$match] ?? self::$uri[$match] ?? $match;
