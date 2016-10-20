0dbbc40a622e5b155300b8c0295fcf5cbca7565c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-10-20 10:02:22 +0200

    Draft genetic algorithm interface

---
+++ Math/Optimization/GeneticAlgorithmInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace phpOMS\Math\Optimization;
+
+/**
+ * Web module interface.
+ *
+ * @category   Framework
+ * @package    phpOMS\Module
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+interface GeneticAlgorithmInterface
+{
+	public function mutate($a);
+
+	public function unfitness($a, $b);
+}
