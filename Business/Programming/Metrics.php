d4eea68b119cebf1401502bdfb1c0931e868029c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-08-18 21:12:04 +0200

    Added metrics

---
+++ Business/Programming/Metrics.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+declare(strict_types=1);
+namespace phpOMS\Business\Programming;
+/**
+ * Programming metrics
+ *
+ * @category   Framework
+ * @package    phpOMS\Business
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Metrics {
+    /**
+     * Calculate ABC metric score
+     *
+     * @latex  r = \sqrt{a^{2} + b^{2} + c^{2}}
+     *
+     * @param int $a Assignments
+     * @param int $b Branches
+     * @param int $c Conditionals
+     *
+     * @return int
+     *
+     * @since  1.0.0
+     */
+    public static function abcScore(int $a, int $b, int $c) : int
+    {
+        return (int) sqrt($a*$a+$b*$b+$c*$c);
+    }
+}
