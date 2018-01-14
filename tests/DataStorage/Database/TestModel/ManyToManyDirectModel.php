3245dbd1bc626d4d87b9ac942186031718994e4d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-01-14 15:44:36 +0100

    Make tests part of the repos

---
+++ tests/DataStorage/Database/TestModel/ManyToManyDirectModel.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+
+namespace Tests\PHPUnit\phpOMS\DataStorage\Database\TestModel;
+
+require_once __DIR__ . '/../../../../../../phpOMS/Autoloader.php';
+
+class ManyToManyDirectModel
+{
+    public $id = 0;
+    
+    public $string = 'ManyToManyDirect';
+
+    public $to = 0;
+}
---
+++ tests/Utils/Parser/Markdown/MarkdownTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+
+namespace Tests\PHPUnit\phpOMS\Utils\Parser\Markdown;
+
+require_once __DIR__ . '/../../../../../../phpOMS/Autoloader.php';
+
+use phpOMS\Utils\Parser\Markdown\Markdown;
+use phpOMS\System\File\Local\Directory;
+
+class MarkdownTest extends \PHPUnit\Framework\TestCase
+{
+    public function testParsing()
+    {
+        $files = Directory::list(__DIR__ . '/data');
+
+        foreach ($files as $file) {
+            $data = explode('.', $file);
+
+            if ($data[1] === 'md') {
+                self::assertEquals(
+                    file_get_contents(__DIR__ . '/data/' . $data[0] . '.html'), 
+                    Markdown::parse(file_get_contents(__DIR__ . '/data/' . $data[0] . '.md')),
+                    $file
+                );
+            }
+        }
+    }
+}
+
