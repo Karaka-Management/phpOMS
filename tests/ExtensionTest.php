24f28dc82c01b21d72aa426488be7416b8ed61bb
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-03-30 10:24:24 +0200

    Add extension test

---
+++ tests/ExtensionTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.2
+ *
+ * @package    TBD
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+
+namespace phpOMS\tests;
+
+class ExtensionTest extends \PHPUnit\Framework\TestCase
+{
+    public function testExtension()
+    {
+        self::assertTrue(extension_loaded('mbstring'));
+        self::assertTrue(extension_loaded('curl'));
+        self::assertTrue(extension_loaded('imap'));
+    }
+}
