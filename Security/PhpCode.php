23f24cf467e12f607db2253cb2b8b43fc42aa720
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-04-13 22:10:54 +0200

    Implement disabled functions

--- Security/PhpCode.php
+++ Security/PhpCode.php
@@ -25,6 +25,21 @@ namespace phpOMS\Security;
 class PhpCode
 {
     /**
+     * Disabled functions
+     *
+     * @var array
+     * @since 1.0.0
+     */
+    public static $disabledFunctions = [
+        'apache_child_terminate', 'apache_setenv', 'define_syslog_variables', 'escapeshellarg', 'escapeshellcmd', 'eval',
+        'exec', 'fp', 'fput', 'ftp_connect', 'ftp_exec', 'ftp_get', 'ftp_login', 'ftp_nb_fput', 'ftp_put', 'ftp_raw',
+        'ftp_rawlist', 'highlight_file', 'ini_alter', 'ini_get_all', 'ini_restore', 'inject_code', 'mysql_pconnect',
+        'openlog', 'passthru', 'php_uname', 'phpAds_remoteInfo', 'phpAds_XmlRpc', 'phpAds_xmlrpcDecode',
+        'phpAds_xmlrpcEncode', 'popen', 'posix_getpwuid', 'posix_kill', 'posix_mkfifo', 'posix_setpgid', 'posix_setsid',
+        'posix_setuid', 'posix_uname', 'proc_close', 'proc_get_status',
+    ];
+
+    /**
      * Deprecated functions
      *
      * @var array
