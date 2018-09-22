10f392408c0493c31e6461b5954c7b7945f99247
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-09-22 13:17:50 +0200

    Fix return type hint

--- System/File/Local/Directory.php
+++ System/File/Local/Directory.php
@@ -71,7 +71,7 @@ final class Directory extends FileAbstract implements DirectoryInterface
      * @param string $path   Path
      * @param string $filter Filter
      *
-     * @return array<string>
+     * @return array<string|array>
      *
      * @since  1.0.0
      */
@@ -105,7 +105,7 @@ final class Directory extends FileAbstract implements DirectoryInterface
      * @param string $extension Extension
      * @param string $exclude   Pattern to exclude
      *
-     * @return array<string>
+     * @return array<string|array>
      *
      * @since  1.0.0
      */
