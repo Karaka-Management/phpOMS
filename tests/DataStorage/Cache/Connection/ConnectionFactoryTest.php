c491d2a4ffb831a792864a0d82d1cf270e5f3c18
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-10-06 16:00:13 +0200

    Split factory tests

--- tests/DataStorage/Cache/Connection/ConnectionFactoryTest.php
+++ tests/DataStorage/Cache/Connection/ConnectionFactoryTest.php
@@ -18,17 +18,35 @@ use phpOMS\DataStorage\Cache\CacheType;
 
 class ConnectionFactoryTest extends \PHPUnit\Framework\TestCase
 {
-    public function testCreate()
+    public function testCreateFileCache()
     {
         self::assertInstanceOf(
             \phpOMS\DataStorage\Cache\Connection\FileCache::class,
             ConnectionFactory::create(['type' => CacheType::FILE, 'path' => 'Cache'])
         );
+    }
+
+    public function testCreateMemCached()
+    {
+        if (!extension_loaded('memcached')) {
+            $this->markTestSkipped(
+              'The Memcached extension is not available.'
+            );
+        }
 
         self::assertInstanceOf(
             \phpOMS\DataStorage\Cache\Connection\MemCached::class,
             ConnectionFactory::create(['type' => CacheType::MEMCACHED, 'data' => $GLOBALS['CONFIG']['cache']['memcached']])
         );
+    }
+
+    public function testCreateRedisCache()
+    {
+        if (!extension_loaded('redis')) {
+            $this->markTestSkipped(
+              'The Redis extension is not available.'
+            );
+        }
 
         self::assertInstanceOf(
             \phpOMS\DataStorage\Cache\Connection\RedisCache::class,
