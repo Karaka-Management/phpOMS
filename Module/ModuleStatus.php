d3cfe29e14c72e331bf77db78b9e771c140aeb1b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-01-13 21:40:06 +0100

    Add new status

--- Module/ModuleStatus.php
+++ Module/ModuleStatus.php
@@ -25,6 +25,7 @@ use phpOMS\Stdlib\Base\Enum;
  */
 abstract class ModuleStatus extends Enum
 {
-    public const ACTIVE   = 1;
-    public const INACTIVE = 2;
+    public const ACTIVE    = 1;
+    public const INACTIVE  = 2;
+    public const AVAILABLE = 3;
 }
