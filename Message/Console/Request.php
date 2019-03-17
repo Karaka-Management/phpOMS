8203389ec04792cc70cd8a1c778d7694a36128fe
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-03-17 14:34:51 +0100

    fix lang in console

--- Message/Console/Request.php
+++ Message/Console/Request.php
@@ -70,7 +70,7 @@ final class Request extends RequestAbstract
     private function init() : void
     {
         $lang = \explode('_', $_SERVER['LANG'] ?? '');
-        $this->header->getL11n()->setLanguage($lang[0] ?? 'en');
+        $this->header->getL11n()->setLanguage($lang[0] === '' ? 'en' : $lang[0]);
     }
 
     /**
