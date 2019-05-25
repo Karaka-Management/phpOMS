a62304baaa4caeb9e39f35cd07b493dca325799b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-05-25 11:15:47 +0200

    Fix extension type test

--- tests/System/File/ExtensionTypeTest.php
+++ tests/System/File/ExtensionTypeTest.php
@@ -23,7 +23,7 @@ class ExtensionTypeTest extends \PHPUnit\Framework\TestCase
 {
     public function testEnums() : void
     {
-        self::assertCount(11, ExtensionType::getConstants());
+        self::assertCount(12, ExtensionType::getConstants());
         self::assertEquals(ExtensionType::getConstants(), \array_unique(ExtensionType::getConstants()));
 
         self::assertEquals(1, ExtensionType::UNKNOWN);
@@ -37,5 +37,6 @@ class ExtensionTypeTest extends \PHPUnit\Framework\TestCase
         self::assertEquals(256, ExtensionType::PRESENTATION);
         self::assertEquals(512, ExtensionType::IMAGE);
         self::assertEquals(1024, ExtensionType::EXECUTABLE);
+        self::assertEquals(2048, ExtensionType::DIRECTORY);
     }
 }
