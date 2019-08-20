ea85dd5f258bdbf61169b38a5107c378e9563a88
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-08-20 16:13:05 +0200

    Update Grid.php

--- Algorithm/PathFinding/Grid.php
+++ Algorithm/PathFinding/Grid.php
@@ -1,1 +1,39 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package    phpOMS\Algorithm\PathFinding
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       https://orange-management.org
+ */
+declare(strict_types=1);
 
+namespace phpOMS\Algorithm\PathFinding;
+
+/**
+ * Grid of nodes.
+ *
+ * @package    phpOMS\Algorithm\PathFinding
+ * @license    OMS License 1.0
+ * @link       https://orange-management.org
+ * @since      1.0.0
+ */
+class Grid
+{
+    private array $nodes = [];
+    private ?Node $nullNode = null;
+    
+    public function getNullNode() : Node
+    {
+        return $this->nullNode;
+    }
+    
+    public function setNullNode(Node $nullNode) : void
+    {
+        $this->nullNode = $nullNode;
+    }
+}
