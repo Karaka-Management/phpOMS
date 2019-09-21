a51be51c03c078250ad485eb78a714b5ba5bb5cb
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-09-21 16:12:30 +0200

    fix docblock

--- DataStorage/Database/Query/Expression.php
+++ DataStorage/Database/Query/Expression.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Orange Management
  *
  * PHP Version 7.4
  *
- * @package   TBD
+ * @package   phpOMS\DataStorage\Database\Query
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
