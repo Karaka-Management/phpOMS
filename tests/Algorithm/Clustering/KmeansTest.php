0d1b8f5e653df5aa34481c209ad400b30743b7f2
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-10-17 17:09:27 +0200

    cleanup unused json queries and fix kmeans rng test

--- tests/Algorithm/Clustering/KmeansTest.php
+++ tests/Algorithm/Clustering/KmeansTest.php
@@ -41,13 +41,13 @@ class KmeansTest extends \PHPUnit\Framework\TestCase
 
             $kmeans = new Kmeans($points, 2);
 
-            if (0 === $kmeans->cluster($points[0])->getGroup()
-                && 0 === $kmeans->cluster($points[1])->getGroup()
-                && 1 === $kmeans->cluster($points[2])->getGroup()
-                && 1 === $kmeans->cluster($points[3])->getGroup()
-                && 1 === $kmeans->cluster($points[4])->getGroup()
-                && 1 === $kmeans->cluster($points[5])->getGroup()
-                && 1 === $kmeans->cluster($points[6])->getGroup()
+            if ($kmeans->cluster($points[0])->getGroup() === 0
+                && $kmeans->cluster($points[1])->getGroup() === 0
+                && $kmeans->cluster($points[2])->getGroup() === 1
+                && $kmeans->cluster($points[3])->getGroup() === 1
+                && $kmeans->cluster($points[4])->getGroup() === 1
+                && $kmeans->cluster($points[5])->getGroup() === 1
+                && $kmeans->cluster($points[6])->getGroup() === 1
             ) {
                 $result = true;
 
