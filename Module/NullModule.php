f554feae82f413ef4863b9ee7014aaee42a0cece
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-07-29 23:52:03 +0200

    handle nullmodule function calls

--- Module/NullModule.php
+++ Module/NullModule.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 
 namespace phpOMS\Module;
 
+use phpOMS\Log\FileLogger;
+
 /**
  * Module abstraction class.
  *
@@ -24,5 +26,22 @@ namespace phpOMS\Module;
  */
 final class NullModule extends ModuleAbstract
 {
+    /** {@inheritdoc} */
+    public function __call(string $name, array $arguments): void
+    {
+        self::__callStatic($name, $arguments);
+    }
 
+    /** {@inheritdoc} */
+    public static function __callStatic(string $name, array $arguments): void
+    {
+        FileLogger::getInstance('', false)
+            ->error(
+                FileLogger::MSG_FULL, [
+                    'message' => 'Expected module/controller but got NullModule.',
+                    'line'    => __LINE__,
+                    'file'    => self::class,
+                ]
+            );
+    }
 }
