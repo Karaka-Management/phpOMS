2589f91995a28948f032948e32c0edb1d1b81f79
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-11-10 22:08:32 +0100

    add email rng

---
+++ Utils/RnG/Email.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   phpOMS\Utils\RnG
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace phpOMS\Utils\RnG;
+
+/**
+ * Email generator.
+ *
+ * @package phpOMS\Utils\RnG
+ * @license OMS License 1.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
+ */
+class Email
+{
+    /**
+     * Get a random email.
+     *
+     * @return string
+     *
+     * @since 1.0.0
+     */
+    public static function generateEmail() : string
+    {
+        $count = \count(Text::LOREM_IPSUM) - 1;
+
+        return Text::LOREM_IPSUM[\mt_rand(0, $count)]
+            . '@' . Text::LOREM_IPSUM[\mt_rand(0, $count)]
+            . '.com';
+    }
+}
