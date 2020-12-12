f5e12ba33dc92d3eb9ff3235b266f6d05dc912b5
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-12-12 18:35:05 +0100

    add root namespace

--- Validation/Network/Hostname.php
+++ Validation/Network/Hostname.php
@@ -50,9 +50,9 @@ abstract class Hostname extends ValidatorAbstract
             return false;
         } elseif (\strlen($value) > 2 && \substr($value, 0, 1) === '[' && \substr($value, -1, 1) === ']') {
             return \filter_var(\substr($value, 1, -1), \FILTER_VALIDATE_IP, \FILTER_FLAG_IPV6) !== false;
-        } elseif (\is_numeric(str_replace('.', '', $value))) {
+        } elseif (\is_numeric(\str_replace('.', '', $value))) {
             return \filter_var($value, \FILTER_VALIDATE_IP, \FILTER_FLAG_IPV4) !== false;
-        } elseif (\filter_var('http://' . $value, FILTER_VALIDATE_URL) !== false) {
+        } elseif (\filter_var('http://' . $value, \FILTER_VALIDATE_URL) !== false) {
             return true;
         }
 
