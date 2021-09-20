31efb58ef393d9e0ee2320e5b63dcdc4e592f79b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-09-20 17:18:13 +0200

    Create StatusAbstract.php

---
+++ Application/StatusAbstract.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   phpOMS\Module
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace phpOMS\Module;
+
+use phpOMS\Application\ApplicationInfo;
+use phpOMS\DataStorage\Database\DatabasePool;
+use phpOMS\DataStorage\Database\Query\Builder;
+use phpOMS\System\File\Local\Directory;
+use phpOMS\System\File\Local\File;
+use phpOMS\System\File\PathException;
+use phpOMS\System\File\PermissionException;
+use phpOMS\Utils\ArrayUtils;
+use phpOMS\Utils\Parser\Php\ArrayParser;
+
+/**
+ * Status abstract class.
+ *
+ * This abstraction can be used by modules in order to manipulate their basic status/state.
+ *
+ * @package phpOMS\Module
+ * @license OMS License 1.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
+ */
+abstract class StatusAbstract
+{
+}
