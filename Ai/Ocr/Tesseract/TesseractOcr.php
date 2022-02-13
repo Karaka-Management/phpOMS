263ea88951a350597c0a8e63537e08d7ef010a35
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-02-13 23:58:36 +0100

    autoformat

--- Ai/Ocr/Tesseract/TesseractOcr.php
+++ Ai/Ocr/Tesseract/TesseractOcr.php
@@ -141,25 +141,25 @@ final class TesseractOcr
      * @param string $image     Image path
      * @param array  $languages Languages to use
      * @param int    $psm       Page segmentation mode (0 - 13)
-     *                              0    Orientation and script detection (OSD) only.
-     *                              1    Automatic page segmentation with OSD.
-     *                              2    Automatic page segmentation, but no OSD, or OCR.
-     *                              3    Fully automatic page segmentation, but no OSD. (Default)
-     *                              4    Assume a single column of text of variable sizes.
-     *                              5    Assume a single uniform block of vertically aligned text.
-     *                              6    Assume a single uniform block of text.
-     *                              7    Treat the image as a single text line.
-     *                              8    Treat the image as a single word.
-     *                              9    Treat the image as a single word in a circle.
-     *                              10   Treat the image as a single character.
-     *                              11   Sparse text. Find as much text as possible in no particular order.
-     *                              12   Sparse text with OSD.
-     *                              13   Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.
+     *                          0    Orientation and script detection (OSD) only.
+     *                          1    Automatic page segmentation with OSD.
+     *                          2    Automatic page segmentation, but no OSD, or OCR.
+     *                          3    Fully automatic page segmentation, but no OSD. (Default)
+     *                          4    Assume a single column of text of variable sizes.
+     *                          5    Assume a single uniform block of vertically aligned text.
+     *                          6    Assume a single uniform block of text.
+     *                          7    Treat the image as a single text line.
+     *                          8    Treat the image as a single word.
+     *                          9    Treat the image as a single word in a circle.
+     *                          10   Treat the image as a single character.
+     *                          11   Sparse text. Find as much text as possible in no particular order.
+     *                          12   Sparse text with OSD.
+     *                          13   Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.
      * @param int    $oem       OCR engine modes
-     *                              0    Legacy engine only.
-     *                              1    Neural nets LSTM engine only.
-     *                              2    Legacy + LSTM engines.
-     *                              3    Default, based on what is available
+     *                          0    Legacy engine only.
+     *                          1    Neural nets LSTM engine only.
+     *                          2    Legacy + LSTM engines.
+     *                          3    Default, based on what is available
      *
      * @return string
      *
