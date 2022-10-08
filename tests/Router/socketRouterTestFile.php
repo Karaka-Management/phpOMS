bc3b9154e9ac0097efbcd4aa97f103ae518409b9
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-10-08 21:35:01 +0200

    fix tests

--- tests/Router/socketRouterTestFile.php
+++ tests/Router/socketRouterTestFile.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 return [
     '^.*backend_admin -settings=general.*$' => [
         0 => [
--- tests/Router/webRouteTestCsrf.php
+++ tests/Router/webRouteTestCsrf.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 return [
     '^.*/backend/admin/settings/csrf.*$' => [
         0 => [
